38d9833d3b64358ceb5c2b6aca196cafba18d556
Daniel Garcia <gamdan89@gmail.com>
2015-10-05 21:21:07 -0700

    Restructured files to create package.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gameboard',
+      version='0.1',
+      description='8x8 gameboard for use with chess or checkers',
+      url='http://github.com/gamda/gamboard',
+      author='Daniel Garcia',
+      author_email='gamdan89@gmail.com',
+      license='MIT',
+      packages=['gameboard'],
+      zip_safe=False)
